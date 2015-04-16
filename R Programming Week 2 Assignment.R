7da447db83aee384b2ce56ca14b5e13c82fe47e4
yogarjun77 <yoga.arjunaanand@gmail.com>
2015-04-16 16:38:12 +0800

    Update R Programming Week 2 Assignment.R

--- R Programming Week 2 Assignment.R
+++ R Programming Week 2 Assignment.R
@@ -1,37 +1,33 @@
 pollutantmean <- function (directory= ".", pollutant, id= 1:332){
         ## 'directory' is a character vector of length 1 indicating
         ## the location of the CSV files
-        
-                
+                      
         ## 'pollutant' is a character vector of length 1 indicating
         ## the name of the pollutant for which we will calculate the
         ## mean; either "sulfate" or "nitrate".
-        
-        
+                
         ## 'id' is an integer vector indicating the monitor ID numbers
         ## to be used
         
-                
+               
         ## Return the mean of the pollutant across all monitors list
         ## in the 'id' vector (ignoring NA values)
         
         #Part 1 - Compile all csv into 1 single data table
-        setwd(directory)
-        csv_filename <-list.files()
-        c1<-read.csv(csv_filename[1], header = TRUE, sep = ",")
-        c_comb <- c1
-        for (i in 2:length(csv_filename)){
-                ci <- read.csv(csv_filename[i], header = TRUE, sep = ",")
-                c_comb <- rbind(c_comb, ci)
-        }
-                
-        }
-        
+       dir_path <- paste(getwd(),"/", directory, "/", sep="")
+      csv_filename <-list.files(dir_path)
+     if(length(id)>1){
+        c_comb <- read.csv(paste(dir_path, csv_filename[id[1]], sep = ""), header = TRUE, sep = ",")
+	range <- id[2]:id[length(id)]
+      	        for (i in seq_along(range)){
+	        ci <- read.csv(paste(dir_path, csv_filename[range[i]], sep = ""), header = TRUE, sep = ",")
+                c_comb <- rbind(c_comb, ci)}
+        } 
+        else {
+        c_comb<-read.csv(paste(dir_path, csv_filename[id[1]], sep = ""), header = TRUE, sep = ",")}
+                       
         #Part 2 - calculate mean according to ID range omit NA
 
-sf <- subset(c_comb, ID=id, select = sulfate)
-
-
-mean(sf$sulfate, na.rm = TRUE)
-
+pollutant_mean<- round(mean(c_comb[,pollutant], na.rm=TRUE), digits = 3)
+return(pollutant_mean)
 }
