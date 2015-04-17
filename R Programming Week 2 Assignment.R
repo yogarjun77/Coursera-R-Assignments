e39a709d843226b1388c8d637a0304b0092bf608
yogarjun77 <yoga.arjunaanand@gmail.com>
2015-04-17 14:37:02 +0800

    Update R Programming Week 2 Assignment.R
    pollutantmean and complete

--- R Programming Week 2 Assignment.R
+++ R Programming Week 2 Assignment.R
@@ -1,4 +1,7 @@
-pollutantmean <- function (directory= ".", pollutant, id= 1:332){
+##Function 1 - pollutantmean
+
+pollutantmean <- function (directory= ".", pollutant, id= 1:332)
+	{
         ## 'directory' is a character vector of length 1 indicating
         ## the location of the CSV files
                       
@@ -15,29 +18,69 @@ pollutantmean <- function (directory= ".", pollutant, id= 1:332){
         
         #Part 1 - Compile all csv into 1 single data table
        dir_path <- paste(getwd(),"/", directory, "/", sep="")
-      csv_filename <-list.files(dir_path)
-     if(length(id)>1){
-        c_comb <- read.csv(paste(dir_path, csv_filename[id[1]], sep = ""), header = TRUE, sep = ",")
-	range <- id[2]:id[length(id)]
-      	        for (i in seq_along(range)){
-	        ci <- read.csv(paste(dir_path, csv_filename[range[i]], sep = ""), header = TRUE, sep = ",")
-                c_comb <- rbind(c_comb, ci)}
-        } 
-        else {
-        c_comb<-read.csv(paste(dir_path, csv_filename[id[1]], sep = ""), header = TRUE, sep = ",")}
+       
+       csv_filename <-list.files(dir_path)
+       
+       c_comb <-NULL
+	      		 
+	      		 for (i in seq_along(id)){
+		
+			ci <- read.csv(paste(dir_path, csv_filename[id[i]], sep = ""), header = TRUE, sep = ",")
+                
+                	c_comb <- rbind(c_comb, ci)
+        				
+        					} 
                        
         #Part 2 - calculate mean according to ID range omit NA
 
-pollutant_mean<- round(mean(c_comb[,pollutant], na.rm=TRUE), digits = 3)
-return(pollutant_mean)
-}
+	pollutant_mean<- round(mean(c_comb[,pollutant], na.rm=TRUE), digits = 3)
+	
+	return(pollutant_mean)
+	
+	}
+
+##Function 2 - complete
 
 
+	complete <- function(directory, id = 1:332) 
+	{
+        ## 'directory' is a character vector of length 1 indicating
+        ## the location of the CSV files
 
+        ## 'id' is an integer vector indicating the monitor ID numbers
+        ## to be used
+        
+        ## Return a data frame of the form:
+        ## id nobs
+        ## 1  117
+        ## 2  1041
+        ## ...
+        ## where 'id' is the monitor ID number and 'nobs' is the
+        ## number of complete cases
 
-c_i <- <- read.csv("./specdata/001.csv", header = TRUE, sep = ",", na.strings = "NA")
-id_i <- id <- c_i$ID[1]
-check_i <- complete.cases(c_i)
-nobs_i <- sum(check_i)
-summary_i <- data.frame(c("id" = id_i, "nobs" = nobs_i))
+	dir_path <- paste(getwd(),"/", directory, "/", sep="")
 
+	csv_filename <-list.files(dir_path)
+	
+	complete <- NULL
+                      
+		for(i in seq_along(id))
+			
+				{
+
+			c_i <- read.csv(paste(dir_path, csv_filename[id[i]], sep = ""), header = TRUE, sep = ",")
+
+			site_i <- c_i$ID[1]
+
+			check_i <- complete.cases(c_i)
+
+			nobs_i <- sum(check_i)
+
+			summary_i <- data.frame(id = site_i, nobs = nobs_i)
+
+			complete <- rbind(complete, summary_i)
+				}
+
+	return(complete)
+
+	}
